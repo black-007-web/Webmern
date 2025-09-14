d4271b36ddbcee6ab6980645bb594dfbf864158b
Mohammad Farial Bin Shah <mdfarial100@gmail.com>
2025-09-15 01:12:43 +0600

    Update userBooks.js

--- server/routes/userBooks.js
+++ server/routes/userBooks.js
@@ -3,6 +3,7 @@ const { getAllBooks } = require('../controllers/bookController');
 
 const router = express.Router();
 
+// 📚 Public route to fetch all books with Cloudinary-hosted image/pdf URLs
 router.get('/', getAllBooks);
 
 module.exports = router;
